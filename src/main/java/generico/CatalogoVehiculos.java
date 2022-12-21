62139d452c5313fab9261c3b62b2ecc3ab82e99d
FernandoMarOrt <fernandomartinezortiz003@gmail.com>
2022-12-21 13:50:04 +0100

    asdadsadsa

---
+++ src/main/java/generico/CatalogoVehiculos.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package generico;
+
+/**
+ *
+ * @author fer
+ */
+public class Newclass extends CatalogoG {
+
+    public Newclass(int tamanio) {
+        super(tamanio);
+    }
+    
+    
+    
+}
